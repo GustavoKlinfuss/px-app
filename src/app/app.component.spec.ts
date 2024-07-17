caf64d4d960b22e3cbd76a6707db46debea94a81
Gustavo Klinfuss <g.kss@hotmail.com>
2024-07-16 23:51:21 -0300

    Inclusão de build

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'pets-app' title`, () => {
+  it(`should have the 'px-app' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('pets-app');
+    expect(app.title).toEqual('px-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, pets-app');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, px-app');
   });
 });
